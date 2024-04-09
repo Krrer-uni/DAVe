b0bbb25ab762c8cb2ec4e1697c2b481356c8d63e
Piotr Durniat <piotr@durniat.com>
2024-04-09 20:47:57 +0200

    fix paths

--- scripts/make_dependency_graph.py
+++ scripts/make_dependency_graph.py
@@ -1,23 +1,27 @@
 import collections
 import os
-
-import pandas as pd
 import re
 
-def find_dependencies(package_name: str) -> []:
-    filename = "../data/packages/" + package_name + ".rb"
+import pandas as pd
+
+script_dir = os.path.dirname(__file__)
+data_dir = os.path.join(script_dir, "..", "data")
+
+
+def find_dependencies(package_name: str) -> list[str]:
+    filename = os.path.join(data_dir, "packages", f"{package_name}.rb")
     dependencies = []
     try:
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             for line_number, line in enumerate(file, start=1):
                 if ":build" in line or ":test" in line:
-                    continue # Skip dependencies that end users don't need
-                prefix = re.search("(depends_on \")|(uses_from_macos \")", line)
+                    continue  # Skip dependencies that end users don't need
+                prefix = re.search('(depends_on ")|(uses_from_macos ")', line)
                 if prefix:
-                    start = line[prefix.end():]
-                    suffix = re.search("\"", start)
+                    start = line[prefix.end() :]
+                    suffix = re.search('"', start)
                     if suffix:
-                        word = start[:suffix.start()]
+                        word = start[: suffix.start()]
                         dependencies.append(word)
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
@@ -27,7 +31,7 @@ def find_dependencies(package_name: str) -> []:
 # print(find_dependencies(package_name=package_name))
 
 
-def get_dependency_tree(package_name: str) -> [set, set]:
+def get_dependency_tree(package_name: str) -> tuple[set, set]:
     relations = set()
     processed = set()
     dep_queue = collections.deque()
@@ -48,24 +52,25 @@ def get_csv(package_name: str):
     processed, relations = get_dependency_tree(package_name)
     df = pd.DataFrame(list(relations))
     df.rename(columns={0: "package", 1: "dependency"}, inplace=True)
-    df.to_csv("../data/csv/" + package_name + ".csv", index=False)
 
+    csv_path = os.path.join(data_dir, "csv", f"{package_name}.csv")
+    df.to_csv(csv_path, index=False)
 
 
 def get_full_csv():
-    packages = os.listdir("../data/packages")
-    packages = [p[:-3] for p in packages] # strip ".rb"
+    packages_dir = os.path.join(data_dir, "packages")
+    packages = os.listdir(packages_dir)
+    packages = [p[:-3] for p in packages]  # strip ".rb"
     relations = []
     for pkg in packages:
         deps = find_dependencies(pkg)
         for d in deps:
-            relations.append((pkg,d))
+            relations.append((pkg, d))
 
     df = pd.DataFrame(list(relations))
     df.rename(columns={0: "package", 1: "dependency"}, inplace=True)
-    df.to_csv("../data/csv/dependencies.csv", index=False)
-
-
+    csv_path = os.path.join(data_dir, "csv", "dependencies.csv")
+    df.to_csv(csv_path, index=False)
 
 
 if __name__ == "__main__":
